b15d4989dbbfddf0acd04032f0e82e82d87c8e5f
raul Coca <raulcocacamacho@gmail.com>
2024-05-21 21:38:24 +0200

    s1 arrays

---
+++ S1/3.arrays/exercise-4.js
@@ -0,0 +1,6 @@
+const rickAndMortyCharacters = ['Rick', 'Beth', 'Jerry'];
+
+rickAndMortyCharacters.push('Morty');
+rickAndMortyCharacters.push('Summer');
+
+console.log(rickAndMortyCharacters[rickAndMortyCharacters.length - 1]);
---
+++ S1/3.arrays/exercise-5.js
@@ -0,0 +1,9 @@
+Elimina el último elemento del array y muestra el primero y el último por consola.
+
+
+const RickAndMortyCharacters = ["Rick", "Beth", "Jerry", "Morty", "Summer", "Lapiz Lopez"];
+
+RickAndMortyCharacters.pop()
+
+console.log(RickAndMortyCharacters[0]);
+console.log(RickAndMortyCharacters[RickAndMortyCharacters.length - 1]);
