eed1922c31d00bd79e2f77e5e71412c76e2d2629
raul Coca <raulcocacamacho@gmail.com>
2024-05-21 21:41:02 +0200

    S2 methods

--- S1/3.arrays/exercise-5.js
+++ S1/3.arrays/exercise-5.js
@@ -1,9 +1,13 @@
-Elimina el último elemento del array y muestra el primero y el último por consola.
+const RickAndMortyCharacters = [
+    'Rick',
+    'Beth',
+    'Jerry',
+    'Morty',
+    'Summer',
+    'Lapiz Lopez',
+];
 
-
-const RickAndMortyCharacters = ["Rick", "Beth", "Jerry", "Morty", "Summer", "Lapiz Lopez"];
-
-RickAndMortyCharacters.pop()
+RickAndMortyCharacters.pop();
 
 console.log(RickAndMortyCharacters[0]);
 console.log(RickAndMortyCharacters[RickAndMortyCharacters.length - 1]);
