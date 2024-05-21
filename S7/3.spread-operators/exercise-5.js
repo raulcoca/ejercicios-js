0e423e061ebb5f79589c799c7819cf5323cfd37e
raul Coca <raulcocacamacho@gmail.com>
2024-05-21 23:48:39 +0200

    S7 spread operators

---
+++ S7/3.spread-operators/exercise-5.js
@@ -0,0 +1,8 @@
+Dado el siguiente array. Crear una copia de él eliminando la posición 2 pero sin editar el array inicial. De nuevo
+, usando spread operatos.
+
+
+const colors = ['rojo', 'azul', 'amarillo', 'verde', 'naranja']; 
+
+const copyColors = [...colors.slice(0, 2), ...colors.slice(3)];
+console.log(copyColors);
